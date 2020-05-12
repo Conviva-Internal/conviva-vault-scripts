6955520583a287b02a6837226af46092ae07d0ab
Jake Becker <5124251+jakerbeck@users.noreply.github.com>
2020-05-12 14:29:13 -0700

    Update vault.sh

--- vault.sh
+++ vault.sh
@@ -1,115 +1,180 @@
 #!/bin/bash
 
-####################
-# Helper Functions #
-####################
-get_token() {
-    read -p "Vault URL (Example: vault.company.com): " URL
-    read -p "Your Okta Username (Example: you@company.com): " USERNAME
-    read -sp "Your Okta Password (Example: ********): " PASSWORD
-    echo "Approve login through your Okta Verify App"
+#############################
+# Argument Helper Functions #
+#############################
+get_vault_url () {
+    if [ -z $VAULT_URL ]; then
+        echo "Vault URL (Example: vault.company.com): "
+        read VAULT_URL
+    fi
+}
+
+get_vault_username () {
+    if [ -z $VAULT_USERNAME ]; then
+        echo "Your Okta Username (Example: you@company.com): "
+        read VAULT_USERNAME
+    fi
+}
+
+get_vault_password () {
+    if [ -z $VAULT_PASSWORD ]; then
+        echo "Your Okta Password (Example: ********): "
+        read -s VAULT_PASSWORD
+    fi
+    if [ ${VAULT_AUTH_METHOD} == "okta" ]; then
+        echo "Approve login through your Okta Verify Mobile App"
+    fi
+}
+
+get_vault_auth_method () {
+    if [ -z ${VAULT_AUTH_METHOD} ]; then
+        echo "Enter an authentication method (Examples: okta, userpass): "
+        read SECRET_ENGINE
+    fi
+    if [ ${VAULT_AUTH_METHOD} != "okta" ] && [ ${VAULT_AUTH_METHOD} != "userpass" ]; then
+        echo "Invalid auth method.  Options: [okta, userpass]"
+        exit 1
+    fi
+}
+
+get_secret_engine () {
+    if [ -z ${SECRET_ENGINE} ]; then
+        echo "Enter a Secret Engine (Example: databases): "
+        read SECRET_ENGINE
+    fi
+}
+
+get_secret_name () {
+    if [ -z ${SECRET} ]; then
+        echo "Enter a Secret (Example: mysql): "
+        read SECRET
+    fi
+}
+
+get_policy_name () {
+    if [ -z ${POLICY_NAME} ]; then
+        echo "Enter a Secret (Example: mysql-ro): "
+        read POLICY_NAME
+    fi
+}
+
+get_policy_path () {
+    if [ -z ${POLICY_PATH} ]; then
+        echo "Enter a Secret (Example: databases/mysql-dev): "
+        read POLICY_PATH
+    fi
+}
+
+get_policy_capabilities () {
+    if [ -z ${POLICY_CAPABILITIES} ]; then
+        echo "Enter a Secret (Example: create, list, read): "
+        read POLICY_CAPABILITIES
+    fi
+}
+
+###################################
+# Action/Command Helper Functions #
+###################################
+get_token () {
+    get_vault_url
+    get_vault_auth_method
+    get_vault_username
+    get_vault_password
     TOKEN=$(curl -sk \
         --request POST \
-        --data "{\"password\": \"${PASSWORD}\"}" \
-        https://${URL}/v1/auth/okta/login/${USERNAME} \
+        --data "{\"password\": \"${VAULT_PASSWORD}\"}" \
+        https://${VAULT_URL}/v1/auth/${VAULT_AUTH_METHOD}/login/${VAULT_USERNAME} \
             | grep -o '"client_token":"[a-z.A-Z0-9]*' | cut -d '"' -f4
     )
 }
 
-list_secrets() {
-    SECRET_ENGINE=$1
-    if [ -z ${SECRET_ENGINE} ]; then
-        read -p "Enter a Secret Engine (Example: services): " SECRET_ENGINE
-    fi
+display_token () {
+    echo "${TOKEN}"
+}
+
+list_secrets () {
+    get_secret_engine
     curl -sk \
         --header "X-Vault-Token: ${TOKEN}" \
         --request LIST \
-        https://${URL}/v1/${SECRET_ENGINE}/metadata \
+        https://${VAULT_URL}/v1/${SECRET_ENGINE}/metadata \
             | grep -o "\[.*\]"
 }
 
-get_secret() {
-    SECRET_ENGINE=$1
-    SECRET=$2
-    if [ -z ${SECRET_ENGINE} ]; then
-        read -p "Enter a Secret Engine (Example: services): " SECRET_ENGINE
-    fi
-    if [ -z ${SECRET} ]; then
-        read -p "Enter a Secret Name (Example: service): " SECRET
-    fi
+get_secret () {
+    get_secret_engine
+    get_secret_name
     curl -sk \
         --header "X-Vault-Token: ${TOKEN}" \
-        https://${URL}/v1/${SECRET_ENGINE}/data/$SECRET
+        https://${VAULT_URL}/v1/${SECRET_ENGINE}/data/${SECRET}
 }
 
-add_secret() {
-    echo "TODO"
+add_secret () {
+    get_secret_engine
+    get_secret_name
+    echo "Function not implemented yet."
 }
 
-list_users() {
+list_users () {
     curl -sk \
         --header "X-Vault-Token: ${TOKEN}" \
         --request LIST \
-        https://${URL}/v1/auth/userpass/users \
+        https://${VAULT_URL}/v1/auth/userpass/users \
             | grep -o '\[.*\]'
 }
 
-add_service_policy() {
-    POLICY=$1
-    PATH=$2
-    CAPABILTIES=$3
-    if [ -z ${POLICY} ]; then
-        read -p "Policy Name (Example: sql-ro): " POLICY
-    fi
-    if [ -z ${PATH} ]; then
-        read -p "Policy path (Example: sql/sql-ro): " PATH
-    fi
-        if [ -z ${CAPABILTIES} ]; then
-        read -p "Policy capabilities (Example: create, list): " CAPABILTIES
-    fi
+add_service_policy () {
+    get_policy_name
+    get_policy_path
+    get_policy_capabilities
     curl -sk \
         --header "X-Vault-Token: ${TOKEN}" \
         --request PUT \
-        --data "{\"policy\": \"path \"${PATH}\" {capabilities = [${CAPABILITIES}]\"}" \
-        https://${URL}/v1/sys/policy/${POLICY}
+        --data "{\"policy\": \"path \"${POLICY_PATH}\" {capabilities = [${POLICY_CAPABILITIES}]\"}" \
+        https://${VAULT_URL}/v1/sys/policy/${POLICY_NAME}
 }
 
-add_service_user() {
-    NEW_USERNAME=$1
-    NEW_PASSWORD=$2
-    POLICIES=$3
-    if [ -z ${NEW_USERNAME} ]; then
-        read -p "Service Name (Example: sql): " NEW_USERNAME
-    fi
-    if [ -z ${NEW_PASSWORD} ]; then
-        read -sp "Service Password (Example: ********): " NEW_PASSWORD
-    fi
-    if [ -z ${POLICIES} ]; then
-        read -p "Service Policies (Example: sql-readonly): " POLICIES
-    fi
+add_service_user () {
+    get_new_username
+    get_new_password
+    get_policy_name
     curl -sk \
         --header "X-Vault-Token: ${TOKEN}" \
         --request POST \
-        --data "{\"password\": \"${NEW_PASSWORD}\",\"policies\": \"${POLICIES}\"}" \
-        https://${URL}/v1/auth/userpass/users/${NEW_USERNAME}
+        --data "{\"password\": \"${NEW_PASSWORD}\",\"policies\": \"${POLICY_NAME}\"}" \
+        https://${VAULT_URL}/v1/auth/userpass/users/${NEW_USERNAME}
 }
 
 #################
 # Main Function #
 #################
-OPTIONS="get_token, list_secrets, get_secret, list_users"
+if [ -z $1 ]; then echo "Invalid argument."; fi
 
-if [ -z $1 ]; then echo "Invalid argument.  Options: $OPTIONS"; fi
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        display_token         ) ACTION="display_token"            ;;
+        list_secrets          ) ACTION="list_secrets"             ;;
+        get_secret            ) ACTION="get_secret"               ;;
+        add_secret            ) ACTION="add_secret"               ;;
+        list_users            ) ACTION="list_users"               ;;
+        add_service_policy    ) ACTION="add_service_policy"       ;;
+        add_service_user      ) ACTION="add_service_user"         ;;
+        -w|--website|--url    ) VAULT_URL="$2"           ; shift  ;;
+        -a|--auth-method      ) VAULT_AUTH_METHOD="$2"   ; shift  ;;
+        -u|--username         ) VAULT_USERNAME="$2"      ; shift  ;;
+        -p|--password         ) VAULT_PASSWORD="$2"      ; shift  ;;
+        -e|--secret-engine    ) SECRET_ENGINE="$2"       ; shift  ;;
+        -s|--secret           ) SECRET="$2"              ; shift  ;;
+        --policy-name         ) POLICY_NAME="$2"         ; shift  ;;
+        --policy-path         ) POLICY_PATH="$2"         ; shift  ;;
+        --policy-capabilities ) POLICY_CAPABILITIES="$2" ; shift  ;;
+        --new-username        ) NEW_USERNAME="$2"        ; shift  ;;
+        --new-password        ) NEW_PASSWORD="$2"        ; shift  ;;
+        * ) echo "Unknown parameter passed: $1"          ; exit 1 ;;
+    esac
+    shift
+done
 
 get_token
-
-case "$1" in
-    "get_token"          ) echo "${TOKEN}"             ;;
-    "list_secrets"       ) list_secrets $2             ;;
-    "get_secret"         ) get_secret $2 $3            ;;
-    "add_secret"         ) add_secret $2 $3            ;;
-    "list_users"         ) list_users                  ;;
-    "add_service_user"   ) add_service_user $2 $3 $4   ;;
-    "add_service_policy" ) add_service_policy $2 $3 $4 ;;
-    "*"                  ) echo "Invalid argument.  Options: $OPTIONS"
-esac
+$ACTION
